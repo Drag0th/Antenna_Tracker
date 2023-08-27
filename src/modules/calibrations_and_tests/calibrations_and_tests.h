1ba44f798f0232166d840c0d8fd4067c6cf2c933
Karol Szlagowski <Szlagowski.k.j@gmail.com>
2023-08-27 09:30:55 +0200

    Initila rewrok

--- src/modules/calibrations_and_tests/calibrations_and_tests.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <AccelStepper.h>
-#include <Servo.h>
-#include "./modules/data_storage/data_storage.h"
-
-void test_movement(AccelStepper& stepper_motor, Servo& servo_motor);
-void set_tracker_position(telemetry_data *data);
-void rssi_guard(telemetry_data *data);
--- src/modules/kinematics/kinematics.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <AccelStepper.h>
-#include <Servo.h>
-
-void azimuth_movement(AccelStepper& stepper_motor, double deg);
-
-void elevation_movement(Servo& servo_motor, double deg);
