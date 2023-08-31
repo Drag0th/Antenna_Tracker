323bdfffefe3a0d7ea77bbf4d6d97844461ab5bd
Karol Szlagowski <Szlagowski.k.j@gmail.com>
2023-08-31 21:53:43 +0200

    Hotfix2

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,8 @@ void setup() {
   //servo
   servo_motor.attach(SERVO_MOTOR_PIN, 544, 2400);
   //movement check
-  stepper_motor.begin(400, 1);
+  stepper_motor.setSpeedProfile(BasicStepperDriver::LINEAR_SPEED, 500, 500);
+  stepper_motor.begin(120, 1);
   check_motors(stepper_motor, servo_motor);
 }
 //------------------------------------------------------------------------------
