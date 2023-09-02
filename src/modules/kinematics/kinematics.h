9263599c23e502ebace83e0e88ecf24731f9d264
Karol Szlagowski <Szlagowski.k.j@gmail.com>
2023-09-02 20:22:16 +0200

    Hotfix

--- src/modules/kinematics/kinematics.h
+++ src/modules/kinematics/kinematics.h
@@ -7,3 +7,4 @@ void check_motors(A4988 &stepper_motor, Servo &servo_motor);
 float stepper_motor_logic(float input_degrees, uint16_t &current_postion);
 double calculate_azimuth_deg(int32_t object_lat, int32_t object_lon, int32_t tracker_lat, int32_t tracker_lon);
 double calculate_elevation_deg(int32_t object_lat, int32_t object_lon, int32_t object_alt, int32_t tracker_lat, int32_t tracker_lon);
+double convert_to_degrees(int32_t lat_or_lon );
