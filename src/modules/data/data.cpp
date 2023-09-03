813067b0a901e7ff7194c1433edc9237db209ede
Karol Szlagowski <Szlagowski.k.j@gmail.com>
2023-09-03 14:31:25 +0200

    Hotfix

--- src/modules/data/data.cpp
+++ src/modules/data/data.cpp
@@ -4,7 +4,18 @@
 void process_data(telemetry_data *data){
     if(data->data_counter == (TELEMTRY_DATA_SAMPLES - 1)){
 
-        summarize_data(data);
+        data->alt_buffer += data->alt;
+        data->relative_alt_buffer += data->relative_alt;
+        data->hdg_buffer += data->hdg;
+        data->cpu_load_buffer += data->cpu_load;
+        data->drop_rate_comm_buffer += data->drop_rate_comm;
+        data->lat_buffer += data->lat;
+        data->lon_buffer += data->lon;
+        data->satellites_visible_buffer += data->satellites_visible;
+        data->fix_type_buffer += data->fix_type;
+        data->cog_buffer += data->cog;
+        data->vel_buffer += data->vel;
+        data->rssi_buffer += data->rssi;
 
         data->average_alt = data->alt_buffer / TELEMTRY_DATA_SAMPLES;
         data->average_relative_alt = data->relative_alt_buffer / TELEMTRY_DATA_SAMPLES;
@@ -35,27 +46,23 @@ void process_data(telemetry_data *data){
         data->data_counter = 0;
     }
     else{
-        summarize_data(data);
+        
+        data->alt_buffer += data->alt;
+        data->relative_alt_buffer += data->relative_alt;
+        data->hdg_buffer += data->hdg;
+        data->cpu_load_buffer += data->cpu_load;
+        data->drop_rate_comm_buffer += data->drop_rate_comm;
+        data->lat_buffer += data->lat;
+        data->lon_buffer += data->lon;
+        data->satellites_visible_buffer += data->satellites_visible;
+        data->fix_type_buffer += data->fix_type;
+        data->cog_buffer += data->cog;
+        data->vel_buffer += data->vel;
+        data->rssi_buffer += data->rssi;
 
         data->data_counter++;
     }
 };
-
-void summarize_data(telemetry_data *data){
-    data->alt_buffer += data->alt;
-    data->relative_alt_buffer += data->relative_alt;
-    data->hdg_buffer += data->hdg;
-    data->cpu_load_buffer += data->cpu_load;
-    data->drop_rate_comm_buffer += data->drop_rate_comm;
-    data->lat_buffer += data->lat;
-    data->lon_buffer += data->lon;
-    data->satellites_visible_buffer += data->satellites_visible;
-    data->fix_type_buffer += data->fix_type;
-    data->cog_buffer += data->cog;
-    data->vel_buffer += data->vel;
-    data->rssi_buffer += data->rssi;
-};
-
 
 void set_tracker_postion(telemetry_data *data){
     data->tracker_lat = data->average_lat;
